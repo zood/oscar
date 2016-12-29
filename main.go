60e89e8b6c9015a78bb459eb45505eca4c5a73b4
Arash Payan <arash@ara.sh>
2016-12-29 10:40:59 -0800

    Tightening up security

--- main.go
+++ main.go
@@ -18,14 +18,10 @@ var Debug = false
 var defaultCiphers = []uint16{
 	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+	// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	tls.TLS_RSA_WITH_AES_128_CBC_SHA,
 }
 
 func main() {
@@ -83,8 +79,11 @@ func main() {
 		tlsConfig := &tls.Config{}
 		tlsConfig.CipherSuites = defaultCiphers
 		tlsConfig.MinVersion = tls.VersionTLS12
-		tlsConfig.MaxVersion = tls.VersionTLS12
 		tlsConfig.PreferServerCipherSuites = true
+		tlsConfig.CurvePreferences = []tls.CurveID{
+			tls.CurveP256,
+			//tls.X25519, // Go 1.8 only
+		}
 		server.TLSConfig = tlsConfig
 		log.Fatal(server.ListenAndServeTLS(*tlsCert, *tlsKey))
 	} else {
