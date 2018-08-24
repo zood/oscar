69b1f5812547f7a27e4c23d5749d6d679e3536a4
Arash Payan <arash@ara.sh>
2018-08-24 02:53:59 -0700

    Integration test for message delivery

---
+++ exercise/messages_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"zood.xyz/oscar/encodable"
+)
+
+type outboundMessage struct {
+	CipherText encodable.Bytes `json:"cipher_text"`
+	Nonce      encodable.Bytes `json:"nonce"`
+	Urgent     bool            `json:"urgent"`
+	Transient  bool            `json:"transient"`
+}
+
+type inboundMessage struct {
+	ID         uint64          `json:"id"`
+	SenderID   encodable.Bytes `json:"sender_id"`
+	CipherText encodable.Bytes `json:"cipher_text"`
+	Nonce      encodable.Bytes `json:"nonce"`
+	SentDate   int64           `json:"sent_date"`
+}
+
+func TestMessageTransfers(t *testing.T) {
+	userA := createUserOnServer(t)
+	tokenA := login(userA, t)
+	userB := createUserOnServer(t)
+	tokenB := login(userB, t)
+
+	// send a message from A to B
+	outMsg := outboundMessage{
+		CipherText: []byte("There's plenty of money in the banana stand"),
+		Nonce:      []byte("nonce!"),
+		Urgent:     false,
+		Transient:  false,
+	}
+	msgJSON, err := json.Marshal(outMsg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, _ := http.NewRequest(http.MethodPost,
+		apiRoot+"/alpha/users/"+hex.EncodeToString(userB.publicID)+"/messages",
+		bytes.NewReader(msgJSON))
+	req.Header.Add("X-Oscar-Access-Token", tokenA)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Incorrect status code: %d", resp.StatusCode)
+	}
+
+	// user B checks for the message
+	req, _ = http.NewRequest(http.MethodGet, apiRoot+"/alpha/messages", nil)
+	req.Header.Add("X-Oscar-Access-Token", tokenB)
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Incorrect status code: %d", resp.StatusCode)
+	}
+
+	msgs := make([]inboundMessage, 0)
+	if err = json.NewDecoder(resp.Body).Decode(&msgs); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(msgs) != 1 {
+		t.Fatalf("Expecting 1 message. Found %d", len(msgs))
+	}
+	inMsg := msgs[0]
+	if !bytes.Equal(inMsg.CipherText, outMsg.CipherText) {
+		t.Fatal("cipher text mismatch")
+	}
+	if !bytes.Equal(inMsg.Nonce, outMsg.Nonce) {
+		t.Fatal("nonce mismatch")
+	}
+	if !bytes.Equal(inMsg.SenderID, userA.publicID) {
+		t.Fatal("incorrect sender id")
+	}
+	sentDate := time.Unix(inMsg.SentDate, 0)
+	if time.Now().Sub(sentDate).Seconds() > 10 {
+		t.Fatal("invalid sent_date")
+	}
+}
