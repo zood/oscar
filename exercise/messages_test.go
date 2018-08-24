69c8de340c6aaf6cdb524c2120e5a8fbc64ecc16
Arash Payan <arash@ara.sh>
2018-08-24 12:50:00 -0700

    Include message deletion in integration test

--- exercise/messages_test.go
+++ exercise/messages_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -24,6 +25,26 @@ type inboundMessage struct {
 	CipherText encodable.Bytes `json:"cipher_text"`
 	Nonce      encodable.Bytes `json:"nonce"`
 	SentDate   int64           `json:"sent_date"`
+}
+
+func retrieveMessages(token string, t *testing.T) []inboundMessage {
+	req, _ := http.NewRequest(http.MethodGet, apiRoot+"/alpha/messages", nil)
+	req.Header.Add("X-Oscar-Access-Token", token)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Incorrect status code: %d", resp.StatusCode)
+	}
+
+	msgs := make([]inboundMessage, 0)
+	if err = json.NewDecoder(resp.Body).Decode(&msgs); err != nil {
+		t.Fatal(err)
+	}
+
+	return msgs
 }
 
 func TestMessageTransfers(t *testing.T) {
@@ -58,21 +79,7 @@ func TestMessageTransfers(t *testing.T) {
 	}
 
 	// user B checks for the message
-	req, _ = http.NewRequest(http.MethodGet, apiRoot+"/alpha/messages", nil)
-	req.Header.Add("X-Oscar-Access-Token", tokenB)
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("Incorrect status code: %d", resp.StatusCode)
-	}
-
-	msgs := make([]inboundMessage, 0)
-	if err = json.NewDecoder(resp.Body).Decode(&msgs); err != nil {
-		t.Fatal(err)
-	}
+	msgs := retrieveMessages(tokenB, t)
 
 	if len(msgs) != 1 {
 		t.Fatalf("Expecting 1 message. Found %d", len(msgs))
@@ -90,5 +97,23 @@ func TestMessageTransfers(t *testing.T) {
 	sentDate := time.Unix(inMsg.SentDate, 0)
 	if time.Now().Sub(sentDate).Seconds() > 10 {
 		t.Fatal("invalid sent_date")
+	}
+
+	// user B deletes the message from the server
+	req, _ = http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/alpha/messages/%d", apiRoot, inMsg.ID), nil)
+	req.Header.Add("X-Oscar-Access-Token", tokenB)
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Incorrect status code: %d", resp.StatusCode)
+	}
+
+	// retrieve the messages again to make sure that message is deleted
+	msgs = retrieveMessages(tokenB, t)
+	if len(msgs) != 0 {
+		t.Fatalf("The message is still on the server. Num found %d", len(msgs))
 	}
 }
