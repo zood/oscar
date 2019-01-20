5cd986ac147e25083027a455065ea24bc8367b28
Arash Payan <arash@ara.sh>
2019-01-20 14:00:57 -0800

    Update build script to product static builds

--- server/build.sh
+++ server/build.sh
@@ -3,4 +3,5 @@
 set -e
 
 export BUILD_TIME=`date -u +%Y-%m-%d-%I:%M:%S`
-go build -i -ldflags "-X main.ServerBuildTime=$BUILD_TIME -s -w" -o oscar
+# https://github.com/golang/go/issues/26492
+go build -tags 'osusergo netgo' -ldflags '-X main.ServerBuildTime=$BUILD_TIME -extldflags "-fno-PIC -static" -s -w' -buildmode pie -o oscar
