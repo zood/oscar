0585bb293b66cb24b5176e4262f420329323dcc8
Arash Payan <arash@ara.sh>
2019-10-22 22:09:25 -0700

    Fix CORS

--- server/cors.go
+++ server/cors.go
@@ -4,17 +4,14 @@ import "net/http"
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		reqHeaders := r.Header.Get("Access-Control-Request-Headers")
+		if reqHeaders != "" {
+			w.Header().Set("Access-Control-Allow-Headers", reqHeaders)
+		}
 		if r.Method == http.MethodOptions {
-			w.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
-			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-			origin := r.Header.Get("Origin")
-			if origin != "" {
-				w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-			}
-			reqHeaders := r.Header.Get("Access-Control-Request-Headers")
-			if reqHeaders != "" {
-				w.Header().Set("Access-Control-Allow-Headers", reqHeaders)
-			}
 			w.WriteHeader(http.StatusOK)
 			return
 		}
